1baae0ebc61689205db711521c9f4bdb17b7185d
imadraude <59265133+imadraude@users.noreply.github.com>
2020-10-21 23:36:16 +0300

    Update pixelizator.py

--- pixelizator.py
+++ pixelizator.py
@@ -1,22 +1,3 @@
-# -*- coding: utf-8 -*-
-
-#   Friendly Telegram (telegram userbot)
-#   Copyright (C) 2018-2020 The Authors
-
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU Affero General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU Affero General Public License for more details.
-#   если не подписан на t.me/keyzend
-#   твоя мама шлюха
-#   You should have received a copy of the GNU Affero General Public License
-#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 from .. import loader, utils  # pylint: disable=relative-beyond-top-level
 import io
 from PIL import Image, ImageOps
